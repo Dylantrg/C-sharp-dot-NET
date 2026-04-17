9db5f768fdeb56e6c15fde0b0077ad8883c231fb
dylantruong <dtru4726@uni.sydney.edu.au>
2026-04-17 16:51:11 +1000

    Create addTask function

--- First_project/Program.cs
+++ First_project/Program.cs
@@ -1,5 +1,24 @@
 ﻿using System;
 
+public class ToDoList
+{
+    public static string[] strings = new string[10];
+    public static int TaskCount = 0;
+
+    public static void AddTask(string task)
+    {
+        if (TaskCount < strings.Length)
+        {
+            strings[TaskCount] = task;
+            TaskCount++;
+        }
+        else
+        {
+            Console.WriteLine("To-Do List is full. Cannot add more tasks.");
+        }
+    }
+}
+
 public class Program
 {
     public static void Main(string[] args)
