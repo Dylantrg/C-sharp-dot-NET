9395933d9bf71276ece59bfb2f82dff4078c9be2
dylantruong <dtru4726@uni.sydney.edu.au>
2026-04-23 23:15:54 +1000

    Multiple thread

--- First_project/obj/Debug/net10.0/First_project.AssemblyInfo.cs
+++ First_project/obj/Debug/net10.0/First_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db5f768fdeb56e6c15fde0b0077ad8883c231fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+902e2d2af260fe56ed50a1195bf8ad007f55944f")]
 [assembly: System.Reflection.AssemblyProductAttribute("First_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OOP_practice/Program.cs
+++ OOP_practice/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public interface IAnimal
+{
+    void Eat();
+}
+public class Animals : IAnimal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+
+    public virtual void Eat()
+    {
+        Console.WriteLine("Animal is eating");
+    }
+
+}
+public class Dog : Animals
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Woof!");
+    }
+
+    public override void Eat()
+    {
+        Console.WriteLine("Dog is eating dog food");
+    }
+}
+
+public class Cat : Animals
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow!");
+    }
+
+    public override void Eat()
+    {
+        Console.WriteLine("Cat is eating cat food");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Animals dog = new Dog();
+        Animals cat = new Cat();
+        dog.MakeSound();
+        cat.MakeSound();
+        dog.Eat();
+        cat.Eat();
+    }
+}
