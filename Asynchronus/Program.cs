5a5fbe8cecb94f9a05d94ca422cbd59e03009c27
dylantruong <dtru4726@uni.sydney.edu.au>
2026-04-23 22:50:19 +1000

    Practice Asynchronus

---
+++ Asynchronus/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Asynchronous
+{
+    public static async void Download()
+{
+    await Task.Delay(1000);
+    Console.WriteLine("Hello, World!");
+}
+    public static void Main(String[] args)
+    {
+        Download();
+        Console.WriteLine("Downloading...");
+        Thread.Sleep(2000);
+    }
+}
